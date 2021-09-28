67978e7fb4008d755bd3d6d36bc276399c4f301e
ZOLTAN Krizsan <krizsan@gmail.com>
2021-09-28 15:54:47 +0200

    Update ugly.c

--- linter/ugly.c
+++ linter/ugly.c
@@ -1,13 +1,24 @@
+/*
+ * File: hello.c
+ */
 #include <stdlib.h>
+#include <string.h>
 
-  void f(void)
-  {
-     int* x = malloc(10 * sizeof(int));
-     x[10] = 0;        // problem 1: heap block overrun
-  }                    // problem 2: memory leak -- x not freed
-
-  int main(void)
-  {
-     f();
-     return 0;
-  }
+int main(int argc, char *argv[]) {
+    char *string, *string_so_far;
+    int i, length;     length = 0;
+    for(i=0; i<argc; i++) {
+        length += strlen(argv[i])+1;
+        string = malloc(length+1);
+ 
+        /*  * Copy the string built so far. */
+        if(string_so_far != (char *)0)
+            strcpy(string, string_so_far);
+        else *string = '\0';
+        strcat(string, argv[i]);
+        if(i < argc-1) strcat(string, " ");
+        string_so_far = string;
+    }
+    printf("You entered: %s\n", string_so_far);
+    return (0);
+}
